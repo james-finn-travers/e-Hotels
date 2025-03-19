516da2d0590df421532a9e61d851a726be754d65
Michael Sandler <msand012@uottawa.ca>
2025-03-19 13:32:32 -0400

    fixed issues with sql file

--- e-Hotels.sql
+++ e-Hotels.sql
@@ -1,3 +1,5 @@
+SET search_path='e-Hotel';
+
 CREATE TABLE HotelChain (
     ID serial PRIMARY KEY, 
     NumHotels int
@@ -17,7 +19,7 @@ CREATE TABLE ChainPhone (
 
 CREATE TABLE ChainEmail (
     HotelChainID int NOT NULL REFERENCES HotelChain(ID),
-    EmailAddr VARCHAR(255) NOT NULL CHECK (EmailAddr LIKE '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    EmailAddr VARCHAR(255) NOT NULL CHECK (EmailAddr SIMILAR TO '%@%.%'),
     PRIMARY KEY (HotelChainID, EmailAddr)
 );
 
@@ -27,7 +29,7 @@ CREATE TABLE Hotel (
     HotelAddr varchar(20) PRIMARY KEY, 
     NumRooms int, 
     StarRating int CHECK (StarRating<= 5 AND StarRating>=0), 
-    HotelChainID int NOT NULL REFERENCES HotelChain(ID),
+    HotelChainID int NOT NULL REFERENCES HotelChain(ID)
 );
 
 CREATE TABLE HotelPhone (
@@ -38,7 +40,7 @@ CREATE TABLE HotelPhone (
 
 CREATE TABLE HotelEmail (
     HotelAddr varchar(20) NOT NULL REFERENCES Hotel(HotelAddr), 
-    EmailAddr VARCHAR(255) NOT NULL CHECK (EmailAddr LIKE '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    EmailAddr VARCHAR(255) NOT NULL CHECK (EmailAddr SIMILAR TO '%@%.%'),
     PRIMARY KEY (HotelAddr, EmailAddr)
 );
 
@@ -53,21 +55,23 @@ CREATE TABLE Room(
 );
 
 CREATE TABLE InstProblem (
-    HotelAddr varchar(20) NOT NULL REFERENCES Room(HotelAddr), 
-    RoomNum int REFERENCES Room(RoomNum),
+    HotelAddr varchar(20) NOT NULL, 
+    RoomNum int NOT NULL,
     Type varchar(40),
-    PRIMARY KEY (HotelAddr, RoomNum,Type)
+    PRIMARY KEY (HotelAddr, RoomNum, Type),
+	FOREIGN KEY (HotelAddr, RoomNum) REFERENCES Room(HotelAddr, RoomNum)
 );
 
 CREATE TABLE InstAmenity (
-    HotelAddr varchar(20) NOT NULL REFERENCES Room(HotelAddr), 
-    RoomNum int REFERENCES Room(RoomNum),
+    HotelAddr varchar(20) NOT NULL, 
+    RoomNum int NOT NULL,
     Type varchar(40),
-    PRIMARY KEY (HotelAddr, RoomNum,Type)
+    PRIMARY KEY (HotelAddr, RoomNum, Type),
+	FOREIGN KEY (HotelAddr, RoomNum) REFERENCES Room(HotelAddr, RoomNum)
 );
 
 CREATE TABLE Employee(
-    SINOrSSN int PRIMARY KEY CHECK (SINOrSSN >=1000000000 AND SINOrSSN<=999999999),
+    SINOrSSN int PRIMARY KEY CHECK (SINOrSSN >=100000000 AND SINOrSSN<=999999999),
     FirstName varchar(20) NOT NULL, 
     MiddleName varchar(20) NOT NULL, 
     LastName varchar(20) NOT NULL, 
@@ -77,8 +81,8 @@ CREATE TABLE Employee(
 );
 
 CREATE TABLE Manages(
-    SINOrSNN int REFERENCES Employee(SINOrSSN), 
-    HotelAddr varchar(20) REFERENCES Employee(HotelAddr),
+    SINOrSSN int REFERENCES Employee(SINOrSSN), 
+    HotelAddr varchar(20) REFERENCES Hotel(HotelAddr),
     PRIMARY KEY(SINOrSSN, HotelAddr)
 ) ;
 
@@ -93,36 +97,39 @@ CREATE TABLE Customer(
 );
 
 CREATE TABLE Booking(
-    HotelAddr varchar(20) NOT NULL REFERENCES Room(HotelAddr), 
-    RoomNum int REFERENCES Room(RoomNum),
+    HotelAddr varchar(20) NOT NULL, 
+    RoomNum int NOT NULL,
     CheckInDate Date,
-    CustomerID NOT NULL REFERENCES Customer(ID),
+    CustomerID varchar(20) NOT NULL REFERENCES Customer(ID),
     CheckOutDate Date, 
     Duration int,
-    PRIMARY KEY (HotelAddr, RoomNumber, CheckInDate)
+    PRIMARY KEY (HotelAddr, RoomNum, CheckInDate),
+	FOREIGN KEY (HotelAddr, RoomNum) REFERENCES Room(HotelAddr, RoomNum)
 );
 
 
 CREATE TABLE Renting(
-    HotelAddr varchar(20) NOT NULL REFERENCES Room(HotelAddr), 
-    RoomNum int REFERENCES Room(RoomNum),
+    HotelAddr varchar(20) NOT NULL, 
+    RoomNum int NOT NULL,
     CheckInDate Date,
-    CustomerID NOT NULL REFERENCES Customer(ID), 
+    CustomerID varchar(20) NOT NULL REFERENCES Customer(ID),
     CheckOutDate Date, 
     Duration int, 
     Balance decimal(10,2), 
-    Employee-SINOrSSN int NOT NULL REFERENCES Employee(SINOrSSN),
-    PRIMARY KEY (HotelAddr, RoomNumber, CheckInDate)
+    EmployeeSINOrSSN int NOT NULL REFERENCES Employee(SINOrSSN),
+    PRIMARY KEY (HotelAddr, RoomNum, CheckInDate),
+	FOREIGN KEY (HotelAddr, RoomNum) REFERENCES Room(HotelAddr, RoomNum)
 );
 
 CREATE TABLE Payment(
-    HotelAddr varchar(20) NOT NULL REFERENCES Renting(HotelAddr), 
-    RoomNum int REFERENCES Renting(RoomNum),
-    CheckInDate Date REFERENCES Renting(CheckInDate), 
-    CustomerID NOT NULL REFERENCES Customer(ID),
+    HotelAddr varchar(20) NOT NULL, 
+    RoomNum int NOT NULL,
+    CheckInDate Date NOT NULL, 
+    CustomerID varchar(20) NOT NULL REFERENCES Customer(ID),
     PaymentAmount decimal(10,2) CHECK (PaymentAmount>0),
     PaymentDate Date NOT NULL,
-    PRIMARY KEY(HotelAddr, RoomNumber, CheckInDate)
+    PRIMARY KEY(HotelAddr, RoomNum, CheckInDate),
+	FOREIGN KEY (HotelAddr, RoomNum, CheckInDate) REFERENCES Renting(HotelAddr, RoomNum, CheckInDate)
 );
 
 CREATE TABLE Archive(
@@ -130,53 +137,124 @@ CREATE TABLE Archive(
     CheckInDate date,
     CheckOutDate date, 
     Duration int, 
-    Balance decimal(10,2), 
+    --Balance decimal(10,2), 
     Type char(7) CHECK(Type in ('Renting','Booking'))
 );
 
-CREATE TRIGGER archive_renting AFTER INSERT ON Renting
-BEGIN
-	INSERT INTO Archive Values
-    (0,NEW.CheckInDate,NEW.CheckOutDate,NEW.Duration,NEW.Balance,'Renting');
-END;
 
-CREATE TRIGGER archive_booking AFTER INSERT ON Booking
-BEGIN
-	INSERT INTO Archive Values
-    (0,NEW.CheckInDate,NEW.CheckOutDate,NEW.Duration,NEW.Balance,'Booking');
-END;
 
-CREATE TRIGGER add_payment AFTER INSERT ON Payment
+CREATE FUNCTION archive_renting_function()
+	RETURNS TRIGGER AS 
+$BODY$
+BEGIN
+    INSERT INTO Archive(CheckInDate, CheckOutDate, Duration, Type)
+    VALUES (NEW.CheckInDate, NEW.CheckOutDate, NEW.Duration, 'Renting');
+    RETURN NEW;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER archive_renting 
+AFTER INSERT ON Renting
+FOR EACH ROW
+EXECUTE PROCEDURE archive_renting_function();
+
+
+CREATE FUNCTION archive_booking_function()
+	RETURNS TRIGGER AS 
+$BODY$
+BEGIN
+    INSERT INTO Archive(CheckInDate, CheckOutDate, Duration, Type)
+    VALUES (NEW.CheckInDate, NEW.CheckOutDate, NEW.Duration, 'Booking');
+    RETURN NEW;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER archive_booking 
+AFTER INSERT ON Booking
+FOR EACH ROW
+EXECUTE PROCEDURE archive_booking_function();
+
+
+CREATE FUNCTION add_payment_function()
+	RETURNS TRIGGER AS
+$BODY$
 BEGIN
 	UPDATE Renting
     SET balance = balance - NEW.PaymentAmount
-    WHERE Renting.HotelAddr = NEW.HotelAddr AND Renting.RoomNumber = NEW.RoomNumber AND Renting.CheckInDate = NEW.CheckInDate;
+    WHERE Renting.HotelAddr = NEW.HotelAddr AND Renting.RoomNum = NEW.RoomNum AND Renting.CheckInDate = NEW.CheckInDate;
+	RETURN NEW;
 END;
+$BODY$ LANGUAGE plpgsql;
 
-CREATE TRIGGER add_hotel AFTER INSERT ON Hotel
+CREATE TRIGGER add_payment 
+AFTER INSERT ON Payment
+FOR EACH ROW
+EXECUTE FUNCTION add_payment_function();
+
+
+CREATE FUNCTION add_hotel_function()
+	RETURNS TRIGGER AS
+$BODY$
 BEGIN
 	UPDATE HotelChain
     SET NumHotels = NumHotels + 1
     WHERE HotelChain.ID = NEW.HotelChainID;
+	RETURN NEW;
 END;
+$BODY$ LANGUAGE plpgsql;
 
-CREATE TRIGGER remove_hotel AFTER DELETE ON Hotel
+CREATE TRIGGER add_hotel 
+AFTER INSERT ON Hotel
+FOR EACH ROW
+EXECUTE PROCEDURE add_hotel_function();
+
+
+CREATE FUNCTION remove_hotel_function()
+	RETURNS TRIGGER AS
+$BODY$
 BEGIN
 	UPDATE HotelChain
     SET NumHotels = NumHotels - 1
     WHERE HotelChain.ID = OLD.HotelChainID;
+	RETURN NEW;
 END;
+$BODY$ LANGUAGE plpgsql;
 
-CREATE TRIGGER add_room AFTER INSERT ON Room
+CREATE TRIGGER remove_hotel 
+AFTER DELETE ON Hotel
+FOR EACH ROW
+EXECUTE PROCEDURE remove_hotel_function();
+
+
+CREATE FUNCTION add_room_function()
+	RETURNS TRIGGER AS
+$BODY$
 BEGIN
 	UPDATE Hotel
     SET NumRooms = NumRooms + 1
     WHERE Hotel.HotelAddr  = NEW.HotelAddr;
+	RETURN NEW;
 END;
+$BODY$ LANGUAGE plpgsql;
 
-CREATE TRIGGER remove_room AFTER DELETE ON Room
+CREATE TRIGGER add_room 
+AFTER INSERT ON Room
+FOR EACH ROW
+EXECUTE PROCEDURE add_room_function();
+
+
+CREATE FUNCTION remove_room_function()
+	RETURNS TRIGGER AS
+$BODY$
 BEGIN
 	UPDATE Hotel
     SET NumRooms = NumRooms - 1
-    WHERE Hotel.HotelAddr  = NEW.HotelAddr;
+    WHERE Hotel.HotelAddr  = OLD.HotelAddr;
+	RETURN NEW;
 END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE TRIGGER remove_room 
+AFTER DELETE ON Room
+FOR EACH ROW
+EXECUTE PROCEDURE remove_room_function();
